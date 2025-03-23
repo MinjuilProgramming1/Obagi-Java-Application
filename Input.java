2beeec347e04b17a3fcebb29a6a255bc3c934076
MinjuilProgramming1 <danielalabraba@gmail.com>
2025-03-23 12:40:33 -0700

    My Assignment

---
+++ Input.java
@@ -0,0 +1,29 @@
+public class Input{
+	public static void main (String[], args){
+	    Scanner input = new Scanner(System.in);
+		
+		
+		System.out.print("Enter an integer: ");
+		int c = input.nextInt();
+		
+		
+		System.out.print("Enter an integer: ");
+		int thisIsAVariable = input.nextInt();
+		
+		
+		System.out.print("Enter an integer: ");
+		int q76354 = input.nextInt();
+		
+		
+		System.out.print("Enter an integer: ");
+		int number = input.nextInt();
+		
+		System.out.println("This is a java program");
+		System.out.println("This is a java program");
+		System.out.println("This is a java program");
+		
+		
+		
+		
+	}
+}
